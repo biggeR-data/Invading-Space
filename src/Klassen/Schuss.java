15d5a7e1f4e8e8d16dda92cfa089d222473e59fc
kinzlerma <maximilian.kinzler@gmx.de>
2021-05-14 18:24:15 +0200

    Schuss Extend

--- src/Klassen/Schuss.java
+++ src/Klassen/Schuss.java
@@ -1,4 +1,8 @@
 package Klassen;
 
-public class Schuss {
+public class Schuss extends BeweglicheObjekte {
+    protected Schuss(int xKoor, int yKoor) {
+        super(xKoor, yKoor);
+    }
 }
+//todo Schuss als rechteck erstellen und über x und y Koordinaten bewegen
