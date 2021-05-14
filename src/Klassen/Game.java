73ffc7413197911844839790b1658197a5ca4a77
Jan Moormann <jan@moormann-family.de>
2021-05-14 17:11:13 +0200

    game klasse

---
+++ src/Klassen/Game.java
@@ -0,0 +1,102 @@
+package Klassen;
+
+import java.util.ArrayList;
+
+// todo: Stages? GameOver? Score speichern?
+
+public class Game extends Thread{
+    // Attribute
+    private long zeahlerTakt = 0;
+    private int monsterGeschwindigkeit = 20;
+    private int schussGeschwindigkeit = 10;
+
+    private int score;
+    private boolean gameover = false;
+
+    private ArrayList<Monster> listMonster = new ArrayList<Monster>();
+    private ArrayList<Schuss> listSchuesse = new ArrayList<Schuss>();
+
+    private long lastTickMillis = 0;
+    private int timePerTick = 100; // in milliseconds
+    
+
+    // run
+    public void run(){
+        // spiele bis gameover
+        while(!gameover){
+
+            // warte bis Zeit vergangen
+            while((lastTickMillis + timePerTick) <= System.currentTimeMillis()){
+                try {
+                    this.sleep(0,100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // mache einen neuen tick
+            System.out.println("neuer tick");
+            tick();
+        }
+
+        // gameover function GUI?
+        // save score?
+    }
+
+    // gametick
+    private void tick(){
+        zeahlerTakt ++;
+
+        // jeder Takt
+        bewegeSchuesse();
+        checkMonsterGetroffen();
+
+        // loese neuen Schuss
+        if(zeahlerTakt % schussGeschwindigkeit == 0){
+            loeseNeuenSchuss();
+        }
+
+        // nicht jeden Takt ausführen (bei Monster beschleunigung MonsterGeschwindigkeit ändern)
+        if(zeahlerTakt % monsterGeschwindigkeit == 0){
+
+            bewegeMonster();
+        }
+
+    }
+
+    // private Operationen
+    private void checkMonsterGetroffen(){
+        System.out.println("checke Monster getroffen");
+        // wenn Monster getroffen - entfernen
+        // Score anpassen
+    }
+
+    private void bewegeSchuesse(){
+        System.out.println("bewege Schüsse");
+
+        for(Schuss schuss:listSchuesse) {
+            // bewege
+        }
+        // entferne wenn ausserhalb
+    }
+
+    private void bewegeMonster(){
+        System.out.println("bewege Monster");
+
+        for(Monster monster:listMonster){
+            //bewege Monster
+            // gameover -> true wenn Monster unten
+        }
+    }
+
+    private void loeseNeuenSchuss() {
+        System.out.println("schieße");
+        listSchuesse.add(new Schuss());
+    }
+
+    private void gameover(){
+        System.out.println("Game over!");
+        //punktzahl übergeben
+    }
+
+}
