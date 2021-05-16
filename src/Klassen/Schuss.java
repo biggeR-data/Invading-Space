e4ed29dd824547c31b85e1cda2d1556fc46bd3ad
kinzlerma <maximilian.kinzler@gmx.de>
2021-05-16 18:05:22 +0200

    Schuss zeichneSchwarz + Weiß

--- src/Klassen/Schuss.java
+++ src/Klassen/Schuss.java
@@ -3,8 +3,22 @@ package Klassen;
 import javafx.scene.Group;
 
 public class Schuss extends BeweglicheObjekte {
+    private final double YBEWEGUNG = 1;
+
     protected Schuss(double xKoor, double yKoor, Group root) {
         super(xKoor, yKoor, root);
     }
 }
 //todo Schuss als rechteck erstellen und über x und y Koordinaten bewegen
+
+    public void schiessenHoch() {
+        zeichneSchwarz();
+        this.yKoor =  erhalteYKoor() - YBEWEGUNG;
+        zeichneWeiss();
+    }
+
+    public void schiessenRunter() {
+        zeichneSchwarz();
+        this.yKoor = erhalteYKoor() + YBEWEGUNG;
+        zeichneWeiss();
+    }
