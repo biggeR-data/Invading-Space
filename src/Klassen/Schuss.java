dc6bcbcce0371973aa26dc46c4b8d767cbf180c2
kinzlerma <maximilian.kinzler@gmx.de>
2021-05-18 13:34:52 +0200

    Schuss zeichneSchwarz + Weiß, Höhe und Breite

--- src/Klassen/Schuss.java
+++ src/Klassen/Schuss.java
@@ -7,18 +7,20 @@ public class Schuss extends BeweglicheObjekte {
 
     protected Schuss(double xKoor, double yKoor, Group root) {
         super(xKoor, yKoor, root);
+        setzeBreite(5);
+        setzeHoehe(10);
     }
 }
 //todo Schuss als rechteck erstellen und über x und y Koordinaten bewegen
 
     public void schiessenHoch() {
-        zeichneSchwarz();
+        zeichneSchwarz(erhalteBreite(), erhalteHoehe());
         this.yKoor =  erhalteYKoor() - YBEWEGUNG;
-        zeichneWeiss();
+        zeichneWeiss(erhalteBreite(), erhalteHoehe());
     }
 
     public void schiessenRunter() {
-        zeichneSchwarz();
+        zeichneSchwarz(erhalteBreite(), erhalteHoehe()));
         this.yKoor = erhalteYKoor() + YBEWEGUNG;
-        zeichneWeiss();
+        zeichneWeiss(erhalteBreite(), erhalteHoehe());
     }
